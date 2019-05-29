de06ac36688333b7b0a04bb9e76641febd819e62
Amit Kumar <amit1144576@gmail.com>
2019-05-29 23:05:28 +0200

    uploading code for authentication module

---
+++ app/src/main/java/com/example/prototype1/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.prototype1;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.PublicKey;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public EditText editTextEmail1;
+    public EditText editTextPassword1;
+    public TextView txtview;
+    public TextView textViewLogin;
+    public ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);// activity_main is corresponding XML file
+
+        txtview = (TextView) findViewById(R.id.textViewRegisterHere);
+        textViewLogin = (TextView) findViewById(R.id.textViewLogin);
+        editTextEmail1 = (EditText) findViewById(R.id.editTextEmail1);
+        editTextPassword1 = (EditText) findViewById(R.id.editTextPassword1);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth .getCurrentUser()!=null){
+
+            //go to add tranaction page if user is already loggedin
+            
+        }
+
+        textViewLogin.setOnClickListener(this);
+        txtview.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                 Intent intent = new Intent(MainActivity.this,
+                        Register.class);
+                startActivity(intent); // startActivity allow you to move
+            }
+        });
+    }
+
+
+
+    private void userLogin(){
+
+        String email = editTextEmail1.getText().toString().trim();
+        String password = editTextPassword1.getText().toString().trim();
+
+
+        if(email.isEmpty()){
+            editTextEmail1.setError("Email is required");
+            editTextEmail1.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword1.setError("Password is required");
+            editTextPassword1.requestFocus();
+            return;
+
+        }
+
+        progressDialog.setMessage("Registering..");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>(){
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressDialog.dismiss();
+            }
+
+        });
+
+
+
+
+
+    }
+
+
+    public void onClick(View view) {
+
+        if (view == textViewLogin){
+           userLogin();
+
+        }
+        if(view == txtview) {
+            Intent intent = new Intent(MainActivity.this,
+                    Register.class);
+            startActivity(intent);
+        }
+    }
+}
+
