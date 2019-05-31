5490a26fd55d634ac9b06e0ae0cb4ce345178e04
Sahmady <saiedakbarahmady@gmail.com>
2019-05-31 11:27:11 +0200

    app update

---
+++ MoneyCntl/app/src/main/java/com/example/wmeuroswent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.wmeuroswent;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+}
