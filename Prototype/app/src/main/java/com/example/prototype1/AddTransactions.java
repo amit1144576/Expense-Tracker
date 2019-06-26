ebbf7942987e02d4ee53b1702771f8457c5a4609
kenkai21 <46776314+kenkai21@users.noreply.github.com>
2019-06-26 20:36:40 +0200

    fix

--- Prototype/app/src/main/java/com/example/prototype1/AddTransactions.java
+++ Prototype/app/src/main/java/com/example/prototype1/AddTransactions.java
@@ -2,7 +2,6 @@ package com.example.prototype1;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.prototype1.DatabaseClasses.MyDataBaseClass;
@@ -21,39 +19,25 @@ import com.google.android.gms.tasks.OnCompleteListener;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import static com.example.prototype1.R.id.buttonFilter;
 import static com.example.prototype1.R.id.fab;
 
 public class AddTransactions extends AppCompatActivity {
 
 
-<<<<<<< HEAD
-    MyDataBaseClass objMyDataBaseClass;
-    private FirebaseAuth firebaseAuth;
-
-    private Button buttonShowAddedTrans;
-    private TextView showValues;
-
-=======
     MyDataBaseClass myDB1;
     private FirebaseAuth firebaseAuth;
 
-    private Button buttonShowAddedTrans;
+    Button buttonShowAddedTrans;
     Button button;
->>>>>>> b11d722d05804fe3a5e0063770b2a65ffed3e2f2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_add_transactions);
         myDB1 = new MyDataBaseClass(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        buttonShowAddedTrans = (Button) findViewById(R.id.buttonShowAddedTrans);
-
-        setSupportActionBar(toolbar);
-
+        //buttonShowAddedTrans = (Button) findViewById(R.id.buttonShowAddedTrans);
         button = (Button) findViewById(R.id.buttonFilter);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -65,19 +49,22 @@ public class AddTransactions extends AppCompatActivity {
         });
 
 
-       //fab.setOnClickListener(this);
+        // FloatingActionButton fab = findViewById(R.id.fab);
+        setSupportActionBar(toolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //fab.setOnClickListener(this);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Toast.makeText(AddTransactions.this, "fab is clicked", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(AddTransactions.this, "fab is clicked", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(AddTransactions.this,FillTransDetails.class);
                 startActivity(intent);
 
             }
         });
 
-        //findViewById(R.id.buttonShowAddedTrans).setOnClickListener(this);
     }
 
     @Override
@@ -95,26 +82,6 @@ public class AddTransactions extends AppCompatActivity {
 
         switch (id)
         {
-
-
-            case R.id.category: {
-
-
-               finish();
-                Intent intent = new Intent(AddTransactions.this,Add_category.class);
-                startActivity(intent);
-
-                break;
-
-            }
-
-            case R.id.filter: {
-                //Toast.makeText(this, "Currency is clicked", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, filterByDate.class));
-                break;
-            }
-
-
             case R.id.setting: {
                 //Toast.makeText(this, "setting is clicked", Toast.LENGTH_SHORT).show();
                 break;
@@ -125,14 +92,18 @@ public class AddTransactions extends AppCompatActivity {
                 startActivity(new Intent(this, currency.class));
                 break;
             }
-
+            case R.id.filter: {
+                //Toast.makeText(this, "Currency is clicked", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, filterByDate.class));
+                break;
+            }
             case R.id.logout: {
 
-                   firebaseAuth.signOut();
-                   finish();
-                   startActivity(new Intent(this, MainActivity.class));
-                    Toast.makeText(this, "User Logout success", Toast.LENGTH_SHORT).show();
-                    break;
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                Toast.makeText(this, "User Logout success", Toast.LENGTH_SHORT).show();
+                break;
 
             }
         }
@@ -144,19 +115,60 @@ public class AddTransactions extends AppCompatActivity {
 
 
 
+      /*private void viewTransactions(){
 
 
+        buttonShowAddedTrans.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Cursor res = myDB.getAllData();
+
+                        if(res.getCount()==0){
+                           showMessage("Error","No data found.");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+
+                        while (res.moveToNext()){
+
+                            buffer.append("Amount : " + res.getString(1)+"\n");
+                            buffer.append("Payment : " + res.getString(2)+"\n");
+                            buffer.append("Category : " + res.getString(3)+"\n");
+                            buffer.append("Date : " + res.getString(5)+"\n");
+                            buffer.append("Comments : " + res.getString(4)+"\n\n");
+
+                        }
+
+                        //Show all data
+                       showMessage("Transactions",buffer.toString());
+
+                    }
+
+                }
+        );
 
 
-<<<<<<< HEAD
+        }
+*/
+
+/*
+    public void showMessage(String title,String Message){
+
+        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+
+    }
+*/
 
 
-
-
-    @Override
-=======
-   /* @Override
->>>>>>> b11d722d05804fe3a5e0063770b2a65ffed3e2f2
+/*    @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
@@ -165,11 +177,35 @@ public class AddTransactions extends AppCompatActivity {
 
             case R.id.buttonShowAddedTrans: {
 
-                Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show();
+                Toast.makeText(AddTransactions.this,"Button clicked",Toast.LENGTH_LONG).show();
 
-                startActivity(new Intent(this, ShowValues.class));
+                DatabaseHelper db1 = new DatabaseHelper(this);
 
+                Toast.makeText(AddTransactions.this,"Button clicked",Toast.LENGTH_LONG).show();
 
+                Cursor res = db1.getAllData();
+
+                if (res.getCount() == 0) {
+                    showMessage("Error", "No data found.");
+                    return;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                while (res.moveToNext()) {
+
+                    buffer.append("ID : " + res.getString(0) + "\n");
+                    buffer.append("Amount : " + res.getString(1) + "\n");
+                    buffer.append("Payment : " + res.getString(2) + "\n");
+                    buffer.append("Category : " + res.getString(3) + "\n");
+                    buffer.append("Date : " + res.getString(5) + "\n");
+                    buffer.append("Recurring : " + res.getString(6) + "\n");
+                    buffer.append("Comments : " + res.getString(4) + "\n\n");
+
+                }
+
+                //Show all data
+                showMessage("Transactions", buffer.toString());
                 break;
 
             }
